85ddfc77b76250be7b55720fcc11944676c56e10
Zohar Liran <zohar.liran@autodesk.com>
2014-07-12 19:37:04 -0700

    run migration after creation of devise user model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140713022524) do
+ActiveRecord::Schema.define(version: 20140713023458) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
